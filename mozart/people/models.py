5bd4f3e11f5d28896d0e76652b1d336bd4cb7681
Jean Claude Adams <tech@arrobasoft.net>
2022-03-07 18:07:06 -0300

    update models and fix some init fields

--- mozart/people/models.py
+++ mozart/people/models.py
@@ -18,3 +18,24 @@ class Person(models.Model):
     is_virtual = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Student(models.Model):
+    level = models.CharField(max_length=15)
+    school_from = models.CharField(max_length=255)
+    observation = models.TextField(null=True)
+    is_virtual = models.BooleanField(default=False)
+    grade = models.CharField(max_length=5)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PhoneBook(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    contact_name = models.CharField(max_length=150)
+    phone = models.CharField(max_length=50)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
