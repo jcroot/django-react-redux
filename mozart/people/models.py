5dd91cb7b2fb5ba91dfb80839c4db4ee230bfef0
Jean Claude Adams <tech@arrobasoft.net>
2022-03-08 14:35:00 -0300

    An Abstract Base Class model that provides self-managed created and modified fields.

--- mozart/people/models.py
+++ mozart/people/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from core.models import City
 
+from core.models import TimestampedModel
 
-class Person(models.Model):
+
+class Person(TimestampedModel):
     first_name = models.CharField(max_length=150),
     last_name = models.CharField(max_length=150),
     email = models.EmailField(max_length=100, unique=True),
@@ -16,25 +18,19 @@ class Person(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=10, unique=True),
     is_virtual = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
-class Student(models.Model):
+class Student(TimestampedModel):
     level = models.CharField(max_length=15)
     school_from = models.CharField(max_length=255)
     observation = models.TextField(null=True)
     is_virtual = models.BooleanField(default=False)
     grade = models.CharField(max_length=5)
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
-class PhoneBook(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+class PhoneBook(TimestampedModel):
     contact_name = models.CharField(max_length=150)
     phone = models.CharField(max_length=50)
     person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
